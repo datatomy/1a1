103d68dd9968cb89f6f707a245cb4f0079f04605
Tomy Alvarez <tomasalvarez4815@gmail.com>
2021-07-17 15:01:13 -0300

    version4.2

--- script.js
+++ script.js
@@ -17,28 +17,28 @@ async function getData() {
   date[4].innerHTML = update;
   date[5].innerHTML = update;
   /// Dolar Oficial
-  let oficialCompra = data[0].compra;
+  let oficialCompra = parseFloat(data[0].compra.replace(/,/g, '.')).toFixed(2);
   let spanOfiCompra = document.getElementById("oficialCompra");
   spanOfiCompra.innerHTML = oficialCompra;
-  let oficialVenta = data[0].venta;
+  let oficialVenta = parseFloat(data[0].venta.replace(/,/g, '.')).toFixed(2);
   let spanOfiVenta = document.getElementById("oficialVenta");
   spanOfiVenta.innerHTML = oficialVenta;
   /// Dolar Blue
-  let blueCompra = data[3].compra;
+  let blueCompra = parseFloat(data[3].compra.replace(/,/g, '.')).toFixed(2);
   let spanBlueCompra = document.getElementById("blueCompra");
   spanBlueCompra.innerHTML = blueCompra;
-  let blueVenta = data[3].venta;
+  let blueVenta = parseFloat(data[3].venta.replace(/,/g, '.')).toFixed(2);
   let spanBlueVenta = document.getElementById("blueVenta");
   spanBlueVenta.innerHTML = blueVenta;
   /// Dolar Turista
-  let turistaCompra = data[1].compra;
+  let turistaCompra = parseFloat(data[1].compra.replace(/,/g, '.')).toFixed(2);
   let spanTuristaCompra = document.getElementById("turistaCompra");
   spanTuristaCompra.innerHTML = turistaCompra;
-  let turistaVenta = data[1].venta;
+  let turistaVenta = parseFloat(data[1].venta.replace(/,/g, '.')).toFixed(2);
   let spanTuristaVenta = document.getElementById("turistaVenta");
   spanTuristaVenta.innerHTML = turistaVenta;
   /// Riesgo Pais
-  let riesgo = data[7].ultimo;
+  let riesgo = parseFloat(data[7].ultimo);
   let spanRiesgoPais = document.getElementById("riesgoPais");
   spanRiesgoPais.innerHTML = riesgo;
 };
@@ -54,17 +54,17 @@ async function getData2() {
   const response2 = await fetch(url2);
   const data2 = await response2.json();
   /// Euro Oficial
-  let euroCompra = data2.oficial_euro.value_buy;
+  let euroCompra = data2.oficial_euro.value_buy.toFixed(2);
   let spanEuroCompra = document.getElementById("euroCompra");
   spanEuroCompra.innerHTML = euroCompra;
-  let euroVenta = data2.oficial_euro.value_sell;
+  let euroVenta = data2.oficial_euro.value_sell.toFixed(2);
   let spanEuroVenta = document.getElementById("euroVenta");
   spanEuroVenta.innerHTML = euroVenta;
   /// Euro Blue
-  let euroBlueCompra = data2.blue_euro.value_buy;
+  let euroBlueCompra = data2.blue_euro.value_buy.toFixed(2);
   let spanEuroBlueCompra = document.getElementById("euroBlueCompra");
   spanEuroBlueCompra.innerHTML = euroBlueCompra;
-  let euroBlueVenta = data2.blue_euro.value_sell;
+  let euroBlueVenta = data2.blue_euro.value_sell.toFixed(2);
   let spanEuroBlueVenta = document.getElementById("euroBlueVenta");
   spanEuroBlueVenta.innerHTML = euroBlueVenta;
 };
